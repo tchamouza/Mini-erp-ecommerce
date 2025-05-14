22e052b2407bb4d07da73521be0b45c5d828d8fd
tchamouza <tchamouzabeni68@gmail.com>
2025-05-14 16:26:00 +0000

    update

--- product.php
+++ product.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
+
 <body>
-<header>
+    <header>
         <h1>Commercia</h1>
         <nav>
             <ul>
@@ -18,4 +21,5 @@
     </header>
     <?php include './templates/footer.php'; ?>
 </body>
+
 </html>
